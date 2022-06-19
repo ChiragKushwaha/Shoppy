9af010b92294bcaafe693fa4c272155d602eba26
Chirag Kushwaha <chirag@ip-192-168-0-169.ap-south-1.compute.internal>
2022-06-19 22:01:41 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
+
